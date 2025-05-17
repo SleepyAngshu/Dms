fcede581462572fd5da7600b4732daa9194e44cb
Adidasg <dasguptaaditya55@gmail.com>
2025-05-17 08:24:48 +0600

    Update pharmacy_model.py

--- models/pharmacy_model.py
+++ models/pharmacy_model.py
@@ -1,6 +1,6 @@
 from db import get_db_connection
 
-def get_all_pharmacies():
+def get_all_pharmacies():           #######  ADITYA ########
     """Get all pharmacies"""
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
@@ -23,7 +23,7 @@ def get_all_pharmacies():
         cursor.close()
         conn.close()
 
-def get_nearby_pharmacies(lat, lng, radius=5):
+def get_nearby_pharmacies(lat, lng, radius=5):           #######  ADITYA ########
     """Get pharmacies within a certain radius using Haversine formula"""
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
